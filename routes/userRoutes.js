3f00e293ac24b80e5c7a8f59bb10be4c25e793e3
Candelaria Barjollo <candebarjollo@gmail.com>
2024-11-25 08:42:27 -0300

    cambios

---
+++ routes/userRoutes.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+//Rutas para registrar un nuevo usuario
+
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).send('El usuario ya existe');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, password: hashedPassword });
+
+        await newUser.save();
+        console.log('Usuario registrado: ', newUser);
+        res.status(201).send('Usuario registrado con éxito');
+    } catch(err){
+        console.log('Error al registrar el usuario: ', err);
+        res.status(500).send('Error al registrar el usuario');
+    }
+
+    })
+
+//Ruta para iniciar sesión
+router.post('/login', async(req,res)=>{
+    const {username, password}= req.body;
+    try{
+        const user= await User.findOne({username});
+        if(!user){
+            return res.status(401).send('Usuario no encontrado');
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if(!match){
+            console.error('Contraseña incorrecta para el usuario: ', username);
+            return res.status(401).send('Contraseña incorrecta');
+        }
+        req.session.user= user.username;
+        res.send(`Usuario ${user.username} ha iniciado sesión`);
+    }catch(err){
+        res.status(500).send('Error en el servidor');
+
+    }
+    });
+//Ruta para cerrar sesión
+router.get('/logout', (req,res)=>{
+    req.session.destroy( err =>{
+        if(err){
+            return res.status(500).send('Error al cerrar sesión');
+        }
+        res.send('Sesión cerrada')
+    })
+})
+
+//Ruta protegida
+router.get('/protegida', (req,res)=>{
+    if (req.session.user){
+        res.send(`Bienvenido, ${req.session.user}`);
+    } else{
+        res.status(401).send('No autorizado');
+    }
+})
+
+    module.exports= router;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors'); // Importa cors después de express
+const session =require('express-session');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const turnosRouter = require('./routes/turnos');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config(); // Carga las variables de entorno al inicio
 
@@ -12,15 +14,31 @@ const app = express(); // Inicializa express
 app.use(cors()); // Habilita CORS para todas las solicitudes
 app.use(express.json()); // Middleware para parsear JSON
 
+//Configuración de la sesión
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie : {secure: false}
+}));
+
 console.log("URI de MongoDB:", process.env.MONGODB_URI); // Confirmación de la URI
+
+// Configuración de CORS
+app.use(cors({
+    origin: 'https://final-front-4aqh.onrender.com',  // URL de tu front-end desplegado en Render
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 // Conectar a MongoDB
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("Conectado a MongoDB"))
     .catch((error) => console.error("Error de conexión:", error));
 
-// Usar las rutas de turnos en /api/turnos
+// Usar las rutas de turnos en /api/turnos y users
 app.use('/api/turnos', turnosRouter);
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Servidor de reserva de turnos funcionando');
